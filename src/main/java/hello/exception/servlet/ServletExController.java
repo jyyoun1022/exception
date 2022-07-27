f4bf519026090c2a599b201cc109af17260b1811
younjaeyeal <jyyoun1021@gmail.com>
2022-07-27 20:31:07 +0900

    스프링부트에서 제공하는 오류 페이지

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -24,4 +24,9 @@ public class ServletExController {
     public void error500(HttpServletResponse response) throws IOException{
         response.sendError(500);
     }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException{
+        response.sendError(400);
+    }
 }
